a29e366ad1fb3c1e74a3c2cbe607e66ad24c8507
CH1MP5T0N <philipusadriel@gmail.com>
2023-09-02 19:57:33 +0700

    new hero, next up youtube

--- wonder-astro/src/components/Hero.tsx
+++ wonder-astro/src/components/Hero.tsx
@@ -2,32 +2,21 @@ import React from 'react';
 import logo from '../assets/wonder_logo1.png';
 import inequal from '../assets/sdg_reduced_inequalities.png';
 import edu from '../assets/sdg_quality_education.png';
+import hero from '../assets/wonder_3d.png'
 
 export default function Nav() {
   return (
-    <section className="flex flex-col-reverse lg:flex-row items-center justify-center py-20">
-      <div className="lg:w-1/2 p-28">
-        <h1 className="text-3xl font-extrabold mb-4">Introducing, Wonder Reader</h1>
-        <p className="mb-6">
-          The revolutionary braille device that will change the world
-        </p>
-        <div className='text-center lg:text-left'>
-            <img className='h-24 inline mr-4' src={edu}></img>
-            <img className='h-24 inline' src={inequal}></img>
-        </div>
-      </div>
-      <div className="lg:w-1/2 p-8">
-        <div className="aspect-w-16 aspect-h-9">
-          <iframe 
-            height='400'
-            width='560'
-            className="rounded-lg shadow-lg"
-            title="YouTube Video"
-            src="https://www.youtube.com/embed/JViAbPYdxp4?si=-6KGODB255EXW7qy"
-            allowFullScreen
-          />
-        </div>
-      </div>
-    </section>
+<section className="bg-purple-500 py-20">
+  <div className="container mx-auto flex items-center justify-between px-10 border-4 border-black rounded-lg shadow-lg bg-white w-4/5">
+    <div className="w-1/2">
+      <h1 className="text-4xl font-bold mb-4">Introducing <br/> Wonder Reader</h1>
+      <p className="text-lg mb-4">The revolutionary braille keyboard meant for everyone to learn and grow.</p>
+      <p className="text-sm">As featured on the Google Solution Challenge</p>
+    </div>
+    <div className="w-1/2 flex justify-center">
+      <img src={hero} className=" h-auto" alt="Wonder Reader" />
+    </div>
+  </div>
+</section>
   );
 }
---
+++ wonder-astro/src/components/Youtube.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Youtube() {
+  return (
+    <section>
+        <div className='container flex items-center border-black '>
+        </div>
+    </section>
+  )
+}
+
+export default Youtube
