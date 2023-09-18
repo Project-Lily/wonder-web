5f406c509bae847beaedc84e7990d3b8df7232bc
CH1MP5T0N <philipusadriel@gmail.com>
2023-09-18 16:26:05 +0700

    mobile responsiveness

--- wonder-astro/src/components/Hero.tsx
+++ wonder-astro/src/components/Hero.tsx
@@ -6,17 +6,18 @@ import hero from '../assets/wonder_3d.png'
 
 export default function Nav() {
   return (
-<section className="bg-purple-500 py-20">
-  <div className="container mx-auto flex items-center justify-between px-10 border-4 border-black rounded-lg shadow-lg bg-white w-4/5">
-    <div className="w-1/2">
-      <h1 className="text-4xl font-bold mb-4">Introducing <br/> Wonder Reader</h1>
-      <p className="text-lg mb-4">The revolutionary braille keyboard meant for everyone to learn and grow.</p>
-      <p className="text-sm">As featured on the Google Solution Challenge</p>
+<section className="bg-purple-500 py-10 md:py-20">
+  <div className="container mx-auto flex flex-col-reverse md:flex-row items-center justify-center md:justify-between px-4 md:px-10 border-4 border-black rounded-lg shadow-lg bg-white w-4/5">
+    <div className="w-full md:w-1/2 text-center md:text-left">
+      <h1 className="text-2xl md:text-4xl font-bold mb-4">Introducing<br />Wonder Reader</h1>
+      <p className="text-base md:text-lg mb-4">The revolutionary braille keyboard meant for everyone to learn and grow.</p>
+      <p className="text-sm mb-4">As featured on the Google Solution Challenge</p>
     </div>
-    <div className="w-1/2 flex justify-center">
-      <img src={hero} className=" h-auto" alt="Wonder Reader" />
+    <div className="w-full md:w-1/2 flex justify-center">
+      <img src={hero} className="w-full md:h-auto" alt="Wonder Reader" />
     </div>
   </div>
 </section>
+
   );
 }
--- wonder-astro/src/components/Youtube.tsx
+++ wonder-astro/src/components/Youtube.tsx
@@ -4,7 +4,7 @@ function Youtube() {
   return (
     <section>
         <div className='mx-auto bg-gradient-to-b from-purple-500 to-white to-50%'>
-            <iframe className='mx-auto border-black border-4 p-7 rounded-lg bg-white' width="650" height="400" src="https://www.youtube.com/embed/JViAbPYdxp4?si=UnA2XHeCDHyqPzFr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            <iframe className='mx-auto border-black border-4 p-2 md:p-7 rounded-lg bg-white w-4/5 md:w-1/2 aspect-video' src="https://www.youtube.com/embed/JViAbPYdxp4?si=UnA2XHeCDHyqPzFr" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
     </section>
   )
