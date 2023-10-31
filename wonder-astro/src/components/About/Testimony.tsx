67e5a851c03574fa91f17d8a5b01f3e1d1dd291f
CH1MP5T0N <philipusadriel@gmail.com>
2023-10-31 17:29:30 +0700

    FIREBASE

---
+++ wonder-astro/src/components/About/Testimony.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Testimony() {
+  return (
+    <div className='h-screen '>
+        Wonder Reader is the most amazing device ever, everybody should be using it!
+    </div>
+  )
+}
+
+export default Testimony
