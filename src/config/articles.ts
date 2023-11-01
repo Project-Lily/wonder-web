8104c1ab04e067e92eaffdc1eceec6301b92e34d
digaji <71363607+digaji@users.noreply.github.com>
2023-11-01 22:32:01 +0700

    feat(index): Add temp article data

---
+++ src/config/articles.ts
@@ -0,0 +1,53 @@
+export const articles = [
+  {
+    img: {
+      url: "src/assets/articles/article-1.webp",
+      alt: "article-1",
+    },
+    type: "Conference",
+    title: {
+      en: "Wonder Reader at ICEVI 2023!",
+      id: "Wonder Reader di ICEVI 2023!",
+    },
+    date: 1694952000,
+    author: { name: "Jason Jeremy", profile: "src/assets/articles/author-1.webp" },
+    content: {
+      en: "",
+      id: "",
+    },
+  },
+  {
+    img: {
+      url: "src/assets/articles/article-2.webp",
+      alt: "article-2",
+    },
+    type: "Conference",
+    title: {
+      en: "Wonder Reader at ICEVI 2024!",
+      id: "Wonder Reader di ICEVI 2024!",
+    },
+    date: 1710244800,
+    author: { name: "Jason Christian", profile: "src/assets/articles/author-2.webp" },
+    content: {
+      en: "",
+      id: "",
+    },
+  },
+  {
+    img: {
+      url: "src/assets/articles/article-3.webp",
+      alt: "article-3",
+    },
+    type: "Conference",
+    title: {
+      en: "Refreshable Braille Cells and Sustainability",
+      id: "Sel Braille Refreshable dan Keberlanjutan",
+    },
+    date: 1697371200,
+    author: { name: "Aric Hernando", profile: "src/assets/articles/author-3.webp" },
+    content: {
+      en: "",
+      id: "",
+    },
+  },
+];
